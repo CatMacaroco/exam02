031dc1371c30f15609c3fcbe4cb4ea983ccdc72a
Catarina Maçaroco <catarinamacaroco@MacBook-Pro-6.local>
2025-12-05 21:08:02 +0100

    practice

--- level 3/hidenp.c
+++ level 3/hidenp.c
@@ -4,30 +4,24 @@ int main(int ac, char *av[])
 {
     int i;
     int j;
-
-    i = 0;
     j = 0;
+    i = 0;
     if(ac == 3)
     {
+        while(av[1][i] && av[2][j])
+        {
+            if(av[1][i] == av[2][j])
+                i++;
+            j++;
+        }
         if(av[1][i] == '\0')
         {
             write(1, "1", 1);
             return(0);
         }
-        while(av[2][j])
-        {
-            if(av[1][i] == av[2][j])
-            {
-                i++;
-            }
-            j++;
-            if(av[1][i] == '\0')
-            {
-                write(1, "1", 1);
-                return(0);
-            }
-        }
-        write(1, "0", 1);
+        else
+            write(1, "0", 1);
     }
     write(1, "\n", 1);
+    return(0);
 }
